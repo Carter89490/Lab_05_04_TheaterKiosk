b26ebedd84e02c52322ffdc9ff5cbe64ec65128e
Carter Holsinger <113468540+Carter89490@users.noreply.github.com>
2022-10-04 14:30:05 -0400

    Add files via upload

---
+++ Lab_05_04_TheaterKiosk/src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age = 19;
+        if(age >= 21)
+            System.out.println("you get a wrist band");
+    }
+}
